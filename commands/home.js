3e043ac79d35cb0956a1fd49c630628e2c78fb09
Lab-lesson <73537376+Lab-lesson@users.noreply.github.com>
2021-01-28 21:14:02 +0800

    Update home.js
    Changed home system to Sol

--- commands/home.js
+++ commands/home.js
@@ -1,27 +1,30 @@
 exports.run = async(client, message, args, level) => {
 	const embed = {
-		"title": "59 Virginis, Alchemia Virginis, The Tavern",
+		"title": "Sol",
 		"color": 4751592,
-		"image": {
-			"url": "https://i.imgur.com/YPWtsgw.png"
-		},
-		"author": {
-			"name": "== Alchemy Den =="
-		},
+		//"image": {
+		//	"url": "https://i.imgur.com/YPWtsgw.png"
+		//},
+		//"author": {
+		//	"name": "== Alchemy Den =="
+		//},
 		"fields": [{
 				"name": "EDDB Links:",
 				"value": "System: https://eddb.io/system/268\nStation: https://eddb.io/station/1521"
-			}, {
-				"name": "Nearest Material Traders:",
-				"value": "**Encoded**: *LFT 1103*, Kirk Ring, 6 Ls, 18.52 Ly\n**Manufactured**: *LP 620-3*, Jensen Enterprise,  14 Ls, 18.21 Ly\n**Raw**: *Chaac*, Lem Dock, 560 Ls, 28.39 Ly"
-			}, {
-				"name": "Nearest Tech Brokers: ",
-				"value": "**Guardian**: *GQ Virginis*, Ray Enterprise, 433 Ls, 27.99 Ly\n**Human**: *Xevioso*,  Ciferri Gateway, 30 Ls, 14.73 Ly"
-			}, {
-				"name": "Nearest Interstellar Factors: ",
-				"value": "*Wolf 497*, Buffett Vista,  56 Ls, 16.94 Ly"
-			}
+			}, 
 		]
+		
+		//{
+				//"name": "Nearest Material Traders:",
+				//"value": "**Encoded**: *LFT 1103*, Kirk Ring, 6 Ls, 18.52 Ly\n**Manufactured**: *LP 620-3*, Jensen Enterprise,  14 Ls, 18.21 Ly\n**Raw**: *Chaac*, Lem Dock, 560 Ls, 28.39 Ly"
+		//	}, {
+				//"name": "Nearest Tech Brokers: ",
+				//"value": "**Guardian**: *GQ Virginis*, Ray Enterprise, 433 Ls, 27.99 Ly\n**Human**: *Xevioso*,  Ciferri Gateway, 30 Ls, 14.73 Ly"
+		//	}, {
+				//"name": "Nearest Interstellar Factors: ",
+				//"value": "*Wolf 497*, Buffett Vista,  56 Ls, 16.94 Ly"
+		//	}
+		//]
 	};
 	message.channel.send({
 		embed
