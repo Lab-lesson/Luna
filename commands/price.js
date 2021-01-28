1e0beab64604567e7ff9da30240ede890e47ab3b
Lab-lesson <73537376+Lab-lesson@users.noreply.github.com>
2021-01-28 23:51:45 +0800

    Update price.js

--- commands/price.js
+++ commands/price.js
@@ -230,30 +230,30 @@ exports.run = async(client, message, args, level) => {
 		for (var i = 0; i < commodityPage.length; i++) {
 			if (commodityPage[i].toLowerCase().includes("minimum buying stations")) {
 
-				system4 = commodityPage[i + 21].split("<")
+				system4 = commodityPage[i + 22].split("<")
 					system4 = system4[1].split(">")
-					system4webpage = commodityPage[i + 21].split("\"")
+					system4webpage = commodityPage[i + 22].split("\"")
 					// output += "\n\nSystem :: " + system[1].replace("&#039;", "'")
 
-					station4 = commodityPage[i + 19].split("<")
+					station4 = commodityPage[i + 20].split("<")
 					station4 = station4[1].split(">")
-					station4webpage = commodityPage[i + 19].split("\"")
+					station4webpage = commodityPage[i + 20].split("\"")
 
 					// output += "\nStation :: " + station[1].replace("&#039;", "'")
 
-					currentPrice4 = commodityPage[i + 23].split("<")
+					currentPrice4 = commodityPage[i + 24].split("<")
 					currentPrice4 = currentPrice4[1].split(">")
 					// output += "\nCurrent Price :: " + currentPrice[1].replace("&#039;", "'")
 
-					demand4 = commodityPage[i + 29].split("<")
+					demand4 = commodityPage[i + 30].split("<")
 					demand4 = demand4[1].split(">")
 					// output += "\nDemand :: " + demand[1].replace("&#039;", "'")
 
-					landingPad4 = commodityPage[i + 32].split("<")
+					landingPad4 = commodityPage[i + 33].split("<")
 					landingPad4 = landingPad4[1].split(">")
 					// output += "\nLanding Pad :: " + landingPad[1].replace("&#039;", "'")
 
-					lastUpdate4 = commodityPage[i + 36].split("<")
+					lastUpdate4 = commodityPage[i + 37].split("<")
 					lastUpdate4 = lastUpdate4[0].split(">")
 					// output += "\nLast Updated :: " + lastUpdate[1].replace("&#039;", "'")
 
