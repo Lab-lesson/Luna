f8c16b524fcd6bb339d558fc85fb798e55d239d5
Lab-lesson <73537376+Lab-lesson@users.noreply.github.com>
2021-01-28 22:09:32 +0800

    Update price.js
    2nd attempt at fixing 'undefined'. Changed key from "name" to "text"

--- commands/price.js
+++ commands/price.js
@@ -329,8 +329,7 @@ exports.run = async(client, message, args, level) => {
 				"name": `Information on ${name}`,
 				"value": `Max Sell Price: **${MaxSell}** credits\nMin Sell Price: **${MinSell}** credits\n\nMax Buy Price: **${MaxBuy}**\nMin Buy Price: **${MinBuy}**\n\nMax Profit: **${MaxProfit}**\n\nAverage Price: **${averagePrice}** credits\n\nType of Commodity: **${type}**\n\n`
 			}, {
-				"name": 'Maximum Selling Stations'
-				"value": ''
+				"text": 'Maximum Selling Stations'
 			}, {
 				"name": "1",
 				"value": `**[${system1[1].replace("&#039;", "'")}](https://eddb.io${system1webpage[1]})**\n[${station1[1].replace("&#039;", "'")}](https://eddb.io${station1webpage[1]})\nPrice: **${currentPrice1[1]}**\nDemand: **${demand1[1]}**\nPad: **${landingPad1[1]}**\nLast Updated: **${lastUpdate1[1]}**`,
@@ -344,8 +343,7 @@ exports.run = async(client, message, args, level) => {
 				"value": `**[${system3[1].replace("&#039;", "'")}](https://eddb.io${system3webpage[1]})**\n[${station3[1].replace("&#039;", "'")}](https://eddb.io${station3webpage[1]})\nPrice: **${currentPrice3[1]}**\nDemand: **${demand3[1]}**\nPad: **${landingPad3[1]}**\nLast Updated: **${lastUpdate3[1]}**`,
 				"inline": true
 			}, {
-				"name": 'Minimum Buying Stations',
-				"value": ''
+				"text": 'Minimum Buying Stations'
 			}, {
 				"name": "1",
 				"value": `**[${system4[1].replace("&#039;", "'")}](https://eddb.io${system4webpage[1]})**\n[${station4[1].replace("&#039;", "'")}](https://eddb.io${station4webpage[1]})\nPrice: **${currentPrice4[1]}**\nDemand: **${demand4[1]}**\nPad: **${landingPad4[1]}**\nLast Updated: **${lastUpdate4[1]}**`,
